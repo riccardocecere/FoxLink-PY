61ba21cefc64f2fe2d438f5c9743c7f7053793e4
riccardocecere <ric.cecere1@stud.uniroma3.it>
2019-03-18 11:53:37 +0100

    web discovery refactor, remove NoneType error

--- web_discovery/main.py
+++ web_discovery/main.py
@@ -9,6 +9,9 @@ def main():
     sites = searx.web_discovery_with_searx(config['web_discovery']['id_seed_path'],
                                            config['web_discovery']['searx']['num_of_searx_result_pages'],)
 
+    sites_set = set()
+    for elem in sites:
+        sites_set.add(elem)
 
 if __name__ == "__main__":
 
--- web_discovery/web_discovery_searx.py
+++ web_discovery/web_discovery_searx.py
@@ -1,5 +1,7 @@
 #from web_discovery import searx
 #from general_utils import rdd_utils, text_parser
+import types
+
 import searx
 
 
@@ -12,16 +14,11 @@ def web_discovery_with_searx(path,num_of_pages):
         for pageno in range (num_of_pages):
             output = searx.searx_request(seed,pageno)
             list = output['results']
-            list_urls = []
             for result in list:
-                try:
-                    list_urls = list_urls.append(result['url'])
-                except:
-                    ''
-            try:
-                results = results.append(list_urls)
-            except:
-                ''
+                results.append(result['url'])
+
         print('#####################')
+        print(results)
+        print(type(results))
     return results
 
