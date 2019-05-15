3c6c31eab9332329e045418d3af9e9285f2c459c
riccardocecere <ric.cecere1@stud.uniroma3.it>
2019-05-15 10:52:14 +0200

    introduced classifier function

--- classifier/main.py
+++ classifier/main.py
@@ -2,15 +2,22 @@ import kafka_interface
 from kafka import KafkaConsumer
 import os
 from time import sleep
+import classifier
+
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 TOPIC_INPUT = os.environ.get('TOPIC_INPUT')
 TOPIC_OUTPUT = os.environ.get('TOPIC_OUTPUT')
 MESSAGES_PER_SECOND = float(os.environ.get('MESSAGES_PER_SECOND'))
 SLEEP_TIME = 1 / MESSAGES_PER_SECOND
+TRAINING_SET = os.environ.get('TRAINING_SET')
+TRAINING_PARQUET = os.environ.get('TRAINING_PARQUET')
+MODEL = os.environ.get('MODEL')
+
 
 
 def main():
+    model = classifier.load_classifier(MODEL, TRAINING_PARQUET, TRAINING_SET)
     print('Running Consumer')
     try:
         consumer = kafka_interface.connectConsumer(TOPIC_INPUT, KAFKA_BROKER_URL)
@@ -33,8 +40,9 @@ def main():
         if (message_dict != {}):
             for topic, messages in message_dict.items():
                 for message in messages:
-                    print(message.value)
-                    kafka_interface.send_message(producer, TOPIC_OUTPUT, message.value)
+                    if classifier.predict(model, message.value) == 1:
+                        print(message.value)
+                        kafka_interface.send_message(producer, TOPIC_OUTPUT, message.value)
                     # print(transaction)  # DEBUG
 
 if __name__ == '__main__':
